729ad654666b8bfa818859442e1f5866618811b4
Petterson Neves <pettersonkesler@gmail.com>
2023-09-28 02:09:40 -0300

    test: write test for a valid and invalid situation

--- fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -1,8 +1,13 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      assert FizzBuzz.build("numbers.txt") == {:ok, [1, 2, :fizz, 4, :buzz, :fizzbuzz, 16]}
+    end
+
+    test "when a invalid file is provided, returns an error" do
+      assert FizzBuzz.build("invalid.txt") == {:error, "Error reading file enoent"}
+    end
   end
 end
