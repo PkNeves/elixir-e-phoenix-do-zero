5baa0f8b4f292335100f94140b99da54304a1b96
Petterson Neves <pettersonkesler@gmail.com>
2023-09-28 01:12:22 -0300

    feat: transform  string to list

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -5,6 +5,9 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:ok, result}) do
+    list = String.split(result, ",")
+  end
+
   def handle_file_read({:error, reason}), do: reason
 end
