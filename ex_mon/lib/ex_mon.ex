14632ce5911f30c1a2b91a69fd0ef9c7266a7325
Petterson Neves <pettersonkesler@gmail.com>
2023-09-29 00:17:52 -0300

    feat: create function to start game

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -4,4 +4,9 @@ defmodule ExMon do
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
   end
+
+  def start_game(player) do
+    computer = Player.build("Robotinik", :punch, :kick, :heal)
+    ExMon.Game.start(computer, player)
+  end
 end
