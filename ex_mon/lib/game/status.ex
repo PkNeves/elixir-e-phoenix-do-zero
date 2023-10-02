1212de0a18988f7e4143351cd9ffa9fe26b23044
Petterson Neves <pettersonkesler@gmail.com>
2023-10-01 21:11:43 -0300

    feat: create heal function

--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -11,7 +11,7 @@ defmodule ExMon.Game.Status do
     IO.puts("--------------------")
   end
 
-  def print_round_message(%{status: :game_over, turn: player} = game_stats) do
+  def print_round_message(%{status: :game_over, turn: _player} = game_stats) do
     IO.puts("\n=== The game is over. ===\n")
     IO.inspect(game_stats)
     IO.puts("--------------------")
@@ -27,5 +27,9 @@ defmodule ExMon.Game.Status do
 
   def print_move_message(:player, :attack, damage) do
     IO.puts("\n=== The Computer attacked the player dealing #{damage} damage. ===\n")
+  end
+
+  def print_move_message(player, :heal, life) do
+    IO.puts("\n=== The #{player} healed itself to #{life} life points. ===\n")
   end
 end
