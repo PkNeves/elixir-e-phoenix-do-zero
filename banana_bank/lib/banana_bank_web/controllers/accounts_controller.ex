86c5aae3e3277e6989c317001712dea83ece085c
Petterson Neves <pettersonkesler@gmail.com>
2024-02-03 22:46:55 -0300

    feat: use transaction with api router

--- banana_bank/lib/banana_bank_web/controllers/accounts_controller.ex
+++ banana_bank/lib/banana_bank_web/controllers/accounts_controller.ex
@@ -7,11 +7,17 @@ defmodule BananaBankWeb.AccountsController do
 
   def create(conn, params) do
     with {:ok, %Account{} = account} <- Accounts.create(params) do
-      IO.inspect(account)
-
       conn
       |> put_status(:created)
       |> render(:create, account: account)
+    end
+  end
+
+  def transaction(conn, params) do
+    with {:ok, transaction} <- Accounts.transaction(params) do
+      conn
+      |> put_status(:ok)
+      |> render(:transaction, transaction: transaction)
     end
   end
 end
